9d4fce959b09e7a504b897ba52957acd54b6664b
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2019-02-11 22:49:15 -0500

    Having trouble meeting requirements for 4 and 5

---
+++ HWs/HW 3/ECE486_HW3_5.m
@@ -0,0 +1,16 @@
+f = linspace(-0.5,0.5,1001);
+z = exp(1i .* 2 .* pi .* f);
+H1 = (1.0000 + 1.6180.*z.^(-1) + 1.0000.*z.^(-2))./(1 + -1.5371.*z.^(-1) + 0.9025.*z.^(-2));
+H2 = (1.0000 - 0.6180.*z.^(-1) + 1.0000.*z.^(-2))./(1 - 0.8100.*z.^(-2));
+
+H1 = 20*log10(H1);
+H2 = 20*log10(H2);
+H = H1 .* H2;
+
+Fig1 = figure('Position', [200, 75, 850, 600]); % set figure size and location
+plot(f,H); % stem plot
+grid on; % add grid
+set(gca, 'fontsize', 16); % increase font size
+xlabel('frequency, Hz', 'fontsize', 16); % x label
+ylabel('dB', 'fontsize', 16); % y label
+title('Homework 3, problem 4', 'fontsize', 16); % title
